47859a4ad136e9e807fe2d2800bac56e419e104c
Chavamora <sbetomora@gmail.com>
2023-06-15 01:01:18 -0600

    changes to settings.py

--- lifeBalance/settings.py
+++ lifeBalance/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6sy)q8&+j7s)yuuthq8k9oaqse6y=p_sori#vc%oa0x567bawh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- lifeBalance/wsgi.py
+++ lifeBalance/wsgi.py
@@ -15,4 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lifeBalance.settings')
 
 application = get_wsgi_application()
 
-app = application
