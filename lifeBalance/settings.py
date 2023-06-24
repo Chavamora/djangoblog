20e98f3056b3dcff7f2ae4771d12e5131fd9d856
Chavamora <sbetomora@gmail.com>
2023-06-24 01:05:51 -0600

    style changes and stuff 2

--- lifeBalance/settings.py
+++ lifeBalance/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6sy)q8&+j7s)yuuthq8k9oaqse6y=p_sori#vc%oa0x567bawh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
